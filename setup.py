311ef39a6b292162b3909e2dd2e36de657f1a08b
Chris Rossi <chris@archimedeanco.com>
2017-03-24 08:40:15 -0400

    Get started.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+version = '2.0'
+
+INSTALL_REQUIRES=[
+    'mysql-connector',
+]
+
+setup(name='cytunes',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=INSTALL_REQUIRES,
+      entry_points={
+          'console_scripts': [
+              'migrate=cytunes.migrate:main',
+              ],
+          }
+      )
