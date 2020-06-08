2c7b032038b84dc3f35d33a429a86fd9fa80f65a
Chris Rossi <chris@archimedeanco.com>
2020-06-08 13:43:15 -0400

    No longer need to migrate, and those dependencies were suffering from bitrot and could no longer install the Python package.

--- setup.py
+++ setup.py
@@ -4,15 +4,10 @@ version = '2.0'
 
 INSTALL_REQUIRES = [
     'ansible',
-    'awesome-slugify',
     'droplets',
     'Jinja2',
     'markdown',
-    'pytaglib',
     'pyyaml',
-
-    # https://github.com/sanpingz/mysql-connector/issues/3
-    'mysql-connector==2.1.4',  # FU
 ]
 
 setup(
@@ -24,7 +19,6 @@ setup(
     install_requires=INSTALL_REQUIRES,
     entry_points={
         'console_scripts': [
-            'migrate=cytunes.migrate:main',
             'publish=cytunes.publish:main',
         ],
     }
